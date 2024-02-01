bad05bc55472f11b8b9a3ce28baed2086568149c
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2024-02-02 00:58:01 +0100

    all changes done for linting and formatting

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
