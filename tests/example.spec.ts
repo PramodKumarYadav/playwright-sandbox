e905b7776af71b0b07aac9c7f8e640fa4ec25d06
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2024-02-02 22:56:07 +0100

    have both formattign and linting errors

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("has and more more some more title @unit-test", async ({ page }) => {
+test("has title @unit-test", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
