be1094192f3622945fa56c9fcc43967e27dabd9a
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2024-02-02 22:36:33 +0100

    test hooks

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from "@playwright/test";
 
-test("has title @unit-test", async ({ page }) => {
+test('has title @unit-test', async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test("get started link", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Click the get started link.
