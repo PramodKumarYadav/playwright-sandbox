55a0d710fae9e52498cf5c9922f3ddba0b9ac023
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2024-02-02 22:21:46 +0100

    added checks to run also linters and unit tests with each commit.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test("has title @unit-test", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
