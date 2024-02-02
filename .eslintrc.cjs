ace15b4ed64dc09980881f8f6822c1872ab7fc67
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2024-02-03 00:58:58 +0100

    Added playwright plugin in list of linters and removed custom rule for .only. Keeping it commented since it is handy to know how to use it for any other custom rule tha tis not available in playwright linter.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,18 +4,22 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:playwright/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
-  rules: {
-    "no-console": 0,
-    "no-restricted-syntax": [
-      "error",
-      {
-        selector: "CallExpression[callee.property.name='only']",
-        message: "We don't want to leave .only on our tests😱",
-      },
-    ],
-  },
+  // rules: {
+  //   "no-console": 0,
+  //   "no-restricted-syntax": [
+  //     "error",
+  //     {
+  //       selector: "CallExpression[callee.property.name='only']",
+  //       message: "We don't want to leave .only on our tests😱",
+  //     },
+  //   ],
+  // },
 };
