4815c20deb22196c599e7615794a7d0154d18c2c
pramodkumaryadav <pramodyadav027@gmail.com>
2024-07-08 19:11:36 +0200

    Added example to show pixel differences from headed and headless mode.

--- tests/ux-tests/sample-field-visual.test.ts
+++ tests/ux-tests/sample-field-visual.test.ts
@@ -5,5 +5,8 @@ test("Validate get started field @visual-test", async ({ page }) => {
   await expect.soft(page).toHaveTitle(/Playwright/);
 
   const getStarted = page.getByRole("link", { name: "Get started" });
-  await expect.soft(getStarted).toHaveScreenshot({});
+  await expect.soft(getStarted).toHaveScreenshot({
+    maxDiffPixels: 500,
+    maxDiffPixelRatio: 0.1,
+  });
 });
