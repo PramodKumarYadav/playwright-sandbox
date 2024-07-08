e549d0750d25f6c8a67894a1c0f48e153943db03
pramodkumaryadav <pramodyadav027@gmail.com>
2024-07-08 18:59:32 +0200

    add visual test

---
+++ tests/ux-tests/sample-field-visual.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test";
+
+test("Validate get started field @visual-test", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  await expect.soft(page).toHaveTitle(/Playwright/);
+
+  const getStarted = page.getByRole("link", { name: "Get started" });
+  await expect.soft(getStarted).toHaveScreenshot({});
+});
