a4867eb45f50ecc647be51a096c0952c086f668e
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2024-09-04 22:10:08 +0200

    update file

--- utils/decorators.ts
+++ utils/decorators.ts
@@ -3,6 +3,7 @@ import { test } from "@playwright/test";
 // decorators.ts
 // eslint-disable-next-line @typescript-eslint/ban-types
 export function step(target: Function, context: ClassMethodDecoratorContext) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return function replacementMethod(...args: any) {
     const name = this.constructor.name + "." + (context.name as string);
     return test.step(
