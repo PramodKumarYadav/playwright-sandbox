60317712b98f94f6d9c32d5536b9e58f7cdf10d1
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2024-10-28 11:13:35 +0100

    fix test again

--- tests/employee-service/employee-service.spec.ts
+++ tests/employee-service/employee-service.spec.ts
@@ -9,7 +9,7 @@ test.describe("Employee-service", { tag: ["@employee-service"] }, () => {
     await page.goto("https://playwright.dev/");
 
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwrighttt/);
+    await expect(page).toHaveTitle(/Playwright/);
   });
 
   /**
