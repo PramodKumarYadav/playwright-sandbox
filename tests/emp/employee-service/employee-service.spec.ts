084743c01b2b01357927c5389552953f416a3508
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2025-01-20 21:11:01 +0100

    update test

--- tests/emp/employee-service/employee-service.spec.ts
+++ tests/emp/employee-service/employee-service.spec.ts
@@ -9,7 +9,7 @@ import { test, expect } from "@playwright/test";
 test.describe("HEAD1-Employee-service", { tag: ["@employee-service"] }, () => {
   test.describe("HEAD2-Employee-service,", { tag: ["@employee-service"] }, () => {
     test.describe("Employee-service", { tag: ["@employee-service"] }, () => {
-      test("Has a title Playwright", async ({ page }) => {
+      test("Has title Playwright", async ({ page }) => {
         await page.goto("https://playwright.dev/");
 
         // Expect a title "to contain" a substring.
