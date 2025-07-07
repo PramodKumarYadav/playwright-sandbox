9c21a7e329dc95dafab3ae7857ac6544954e75bd
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2025-07-07 21:41:24 +0200

    update tests and config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,30 +58,16 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
+
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
   ],
 
   /* Run your local dev server before starting the tests */
---
+++ tests/load-test-10-mins-total/75-seconds-file06.spec.ts
@@ -0,0 +1,23 @@
+/* eslint-disable playwright/no-wait-for-timeout */
+import { test, expect } from "@playwright/test";
+
+// Define different test durations
+const testDurations = [5, 10, 15, 20, 25]; // seconds
+
+testDurations.forEach((duration) => {
+  test(`Wait for ${duration} seconds`, async ({ page }) => {
+    // Navigate to the page before starting the load test.
+    await page.goto("https://demo.playwright.dev/todomvc");
+
+    // This test simulates a load test that runs for the specified duration.
+    const startTime = Date.now();
+    while (Date.now() - startTime < duration * 1000) {
+      // Do nothing, just wait.
+      await page.waitForTimeout(1000);
+    }
+    // Assert that the test ran for the expected duration.
+    const elapsedTime = Date.now() - startTime;
+    expect(elapsedTime).toBeGreaterThanOrEqual(duration * 1000);
+    console.log(`Load test completed in ${elapsedTime / 1000} seconds`);
+  });
+});
